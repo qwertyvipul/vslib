a2d99b8bff484127a474d3347651287d33f6882f
Vipul Sharma <vipuls526@gmail.com>
2018-09-04 10:34:02 +0530

    added test

---
+++ test/singly-linked-list.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include "../vslib/singly_linked_list.h"
+using namespace std;
+using namespace vip;
+
+int main(){
+	SinglyLinkedList<int> list;
+	list.insert(10);
+	list.insert(11);
+	list.insert(12);
+	list.print();
+	cout<<endl;
+	list.deleteNodeByValue(11);
+	list.print();
+	cout<<endl;
+	cout.setf(ios::boolalpha);
+	cout<<"Q. 10 Exists?: "<<list.valueExist(10)<<endl;
+	cout<<"Q. 11 Exists?: "<<list.valueExist(11)<<endl;
+	return 0;
+}
