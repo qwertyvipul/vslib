154a06d4651780b8fe7ec8e7c5ca242bbea3021b
Vipul Sharma <vipuls526@gmail.com>
2019-04-07 00:07:40 +0530

    Update and rename binary_search_tree.h to binary_search_tree.hpp

--- vslib/binary_search_tree.hpp
+++ vslib/binary_search_tree.hpp
@@ -1,5 +1,5 @@
-#ifndef BINARY_SEARCH_TREE_H
-#define BINARY_SEARCH_TREE_H
+#ifndef BINARY_SEARCH_TREE_HPP
+#define BINARY_SEARCH_TREE_HPP
 
 #include "duplicate_key_exception.h"
 #include<iostream>
